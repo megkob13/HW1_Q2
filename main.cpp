e3a9f6a9e9296bf822c78b74fe603a91c67eaf6a
Megan <mko5258@psu.edu>
2023-10-05 01:18:17 -0400

    Rest of code

--- main.cpp
+++ main.cpp
@@ -21,3 +21,22 @@ int main()
 	double dt = .01; //step size
 	double duration = 5; //total time to find roll rate for
 	double size = duration / dt + 1; // size of array to get time increments
+	double K = 2; //feedback control gain
+	double p[502]; //roll rate array
+	p[0] = 1; //initial roll rate
+
+	for (int i = 0; i < size; i++)
+	{
+		double time = dt * i; //total time
+
+		delta = -K * p[i]; //calculate delta 
+
+		p_dot = Lp * p[i] + Ldelta * delta; //calculate derivative of roll rate
+		p[i + 1] = p[i] + p_dot * dt; //calculate next roll rate 
+
+		cout << fixed;
+		cout << setprecision(2); // prints two decimal points of time and roll rate 
+		cout << "When t = " << time << " seconds, the roll rate = " << p[i] << " radians per second" << endl; // prints every result
+	}
+	return 0;
+}
